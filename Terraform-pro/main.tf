0ec06db4d33bfe0282cdf8ef44df5d333df1602b
Praveen Kumar <praveen246.pk33@gmail.com>
2024-07-03 15:34:51 +0530

    Jenkins update6

--- Terraform-pro/main.tf
+++ Terraform-pro/main.tf
@@ -4,6 +4,15 @@ module "Web-server" {
   
 }
 
+terraform {
+  backend "s3" {
+    bucket         = "terraform0123tf"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-lock"
+  }
+}
+
 output "LB_DNS_NAME" {
   value = module.Web-server.load_balancer_dns
 }
